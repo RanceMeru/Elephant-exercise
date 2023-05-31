f026e947f6f4fbae7ab589828e6bd2fe356a2c30
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-05-31 11:33:27 -0700

    This project demonstrates creating objects and different classes in C# to assign values such as name, height ,and earsize to our elephant objects. The elephant class methods are made public so that the program class can acceess them at anytime.

---
+++ Elephant exercise/Elephant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elephant_exercise
+{
+    class Elephant
+    {   /*goal is to make two elephant objects then give them individual names ,earsize  the* reference values will be swapped*/
+
+        public int EarSize; // theses will be methods that determine the ear size and names
+        public string Name;
+        //void methods I am still not too clear on
+        //the behavior or actions is made by the mdethods
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is " + Name + ".");
+            Console.WriteLine("My ears are " + EarSize + " inches tall.");
+        }
+
+        public void HearMessage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine(Name + " heard a message");
+            Console.WriteLine(whoSaidIt.Name + " said this: " + message);
+        }
+
+        public void SpeakTo(Elephant whoToTalkTo, string message)
+        {
+            whoToTalkTo.HearMessage(message, this);
+        }
+
+    }
+}
+
---
+++ Elephant exercise/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Elephant_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+            //this will display first 
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            while (true)
+            {
+                char input = Console.ReadKey(true).KeyChar;
+                Console.WriteLine("You pressed " + input);
+                if (input == '1')
+                {
+                    Console.WriteLine("Calling lloyd.WhoAmI()");
+                    lloyd.WhoAmI();
+                }
+                else if (input == '2')
+                {
+                    Console.WriteLine("Calling lucinda.WhoAmI()");
+                    lucinda.WhoAmI();
+                }
+                else if (input == '3')
+                {
+                    /* this part is confusing me the most */
+                    Elephant holder;
+                    holder = lloyd;
+                    lloyd = lucinda;
+                    lucinda = holder;
+                    Console.WriteLine("References have been swapped");
+                }
+                else if (input == '4')
+                {
+                    lloyd = lucinda;
+                    lloyd.EarSize = 4321;
+                    lloyd.WhoAmI();
+                }
+                else if (input == '5')
+                {
+                    lucinda.SpeakTo(lloyd, "Hi, Lloyd!");
+                }
+                else return;
+                Console.WriteLine();
+                // the whole time i'm thinking maybe a switch statement couldve been used its right at the limit of 5 and could be simplified a bit
+            }
+        }
+    }
+}
